1f2209f850a08afd3861dcf8eb4a3ec355e397cb
Mazen Alkhalil <mazen.alkhalil@kuwaitnet.email>
2024-11-25 10:28:47 +0400

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Carousel3D :cardsData= fetchedCardsData />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Carousel3D from "./components/carousel3D.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Carousel3D,
+  },
+  data() {
+    return {
+      fetchedCardsData: [
+        { linkText: "show more", imgURL: "./assets/59d25ae975834f222e0d461af717678e.jpg" },
+        { linkText: "show more" },
+        { linkText: "show more" },
+        { linkText: "show more" },
+        { linkText: "show more" },
+        { linkText: "show more" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+:root {
+  --num-of-cards: 6;
+  --slider-width: 250px;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+ul {
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+button {
+  cursor: pointer;
+}
+
+button,
+input {
+  outline: none;
+}
+
+button {
+  border: none;
+  background-color: #fff;
+}
+
+body {
+  font-family: "poppins", system-ui, -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+    sans-serif;
+  line-height: normal;
+  font-style: normal;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background: #330e66;
+  background: linear-gradient(#330e66, #ebdcff, #330e66);
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
---
+++ src/components/carousel3D.vue
@@ -0,0 +1,119 @@
+<template>
+  <!-- bind the swipe event, no matter direction -->
+<span v-touch:swipe="swipeHandler">Swipe Here</span>
+  <div class="container">
+    <div class="carousel-container"  @mousedown="(event) => console.log(event)">
+      <div
+        class="carousel-3D"
+        v-bind:style="{
+          '-webkit-transform': 'rotateY(' + currentDegree + 'deg)',
+          '-moz-transform': 'rotateY(' + currentDegree + 'deg)',
+          '-o-transform': 'rotateY(' + currentDegree + 'deg)',
+          transform: 'rotateY(' + currentDegree + 'deg)',
+          transition: 'transform ease-in-out 1s',
+        }"
+      >
+        <CarouselCard
+          v-for="(card, index) in cardsData"
+          :key="index"
+          :linkText="card.linkText"
+          :imgURL="card.imgURL"
+          :rotationDegrees="(index * 360) / cardsData.length"
+        />
+      </div>
+    </div>
+    <div class="carousel-buttons">
+      <button class="carousel-btn" v-on:click="rotate('prev')">
+        &#129028;
+      </button>
+      <button class="carousel-btn" v-on:click="rotate('next')">
+        &#129030;
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import CarouselCard from "./carouselCard.vue";
+import Vue2TouchEvents from 'vue2-touch-events'
+
+export default {
+  name: "carousel3D",
+  components: {
+    CarouselCard,
+  },
+  props: {
+    cardsData: Array,
+  },
+  data() {
+    return {
+      currentDegree: 0,
+    };
+  },
+  methods: {
+    rotate: function (direction) {
+      if (direction === "prev") {
+        this.currentDegree = this.currentDegree + 360 / this.cardsData.length;
+        
+      } else {
+        this.currentDegree = this.currentDegree - 360 / this.cardsData.length;
+        
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  position: relative;
+}
+
+.carousel-container {
+  margin: 90px auto 150px;
+  width: var(--slider-width);
+  height: 200px;
+  position: relative;
+  cursor: grab;
+  transform-style: preserve-3d;
+  perspective: 1000px;
+  transform: rotateX(-7deg);
+}
+
+.carousel-3D {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+}
+
+.carousel-buttons {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 740px;
+  margin-inline: auto;
+  padding-inline: 20px;
+  pointer-events: none;
+}
+
+.carousel-btn {
+  background: linear-gradient(180deg, #fff 0%, #c0a3de 100%);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  pointer-events: all;
+  font-weight: 700;
+  font-size: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #48038f;
+}
+</style>
